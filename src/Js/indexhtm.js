f83dc6a2384757877e5aea9c8e748bf59c584723
NoeLiceaga <noe_tetraodron@outlook.com>
2021-04-03 22:38:12 -0600

    Algunas animaciones agregadas

--- src/Js/indexhtm.js
+++ src/Js/indexhtm.js
@@ -1,45 +1,44 @@
-const { ipcRenderer , remote} = require('electron');
-const products = document.querySelector('#products');
-const botton = document.querySelector('.nav-toggle');
-const index = remote.require('./index');
-const person = document.getElementById('person');
+const { ipcRenderer, remote } = require("electron");
+const products = document.querySelector("#products");
+const botton = document.querySelector(".nav-toggle");
+const index = remote.require("./index");
+const person = document.getElementById("person");
 
-person.addEventListener('click',addPerson)
+person.addEventListener("click", addPerson);
 
-function addPerson(){
-    index.createNewProduct()    
+function addPerson() {
+  index.createNewProduct();
 }
 
 // ==== RESPONSIVE NAVBAR === //
- const showNavbar = (toggleId, navId, bodyId, headerId)=>{
-     const toggle = document.getElementById(toggleId),
-     nav = document.getElementById(navId),
-     bodypd = document.getElementById(bodyId),
-     headerpd = document.getElementById(headerId);
+const showNavbar = (toggleId, navId, bodyId, headerId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId),
+    bodypd = document.getElementById(bodyId),
+    headerpd = document.getElementById(headerId);
 
-     if(toggle && nav && bodypd && headerpd){
-        toggle.addEventListener('click',()=>{
+  if (toggle && nav && bodypd && headerpd) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
 
-            nav.classList.toggle('show')
+      toggle.classList.toggle("bx-x");
 
-            toggle.classList.toggle('bx-x')
+      bodypd.classList.toggle("body-pd");
 
-            bodypd.classList.toggle('body-pd')
+      headerpd.classList.toggle("body-pd");
+    });
+  }
+};
 
-            headerpd.classList.toggle('body-pd')
-        })
-     }
- }
+showNavbar("header-toggle", "nav-bar", "body-pd", "header");
 
- showNavbar('header-toggle','nav-bar','body-pd','header');
+const linkColor = document.querySelectorAll(".nav__link");
 
- const linkColor = document.querySelectorAll('.nav__link');
+function colorLink() {
+  if (colorLink) {
+    linkColor.forEach((l) => l.classList.remove("active"));
+    this.classList.add("active");
+  }
+}
 
- function colorLink(){
-     if(colorLink){
-         linkColor.forEach(l => l.classList.remove('active'))
-         this.classList.add('active')
-     }
- }
-
- linkColor.forEach(l=> l.addEventListener('click',colorLink))
+linkColor.forEach((l) => l.addEventListener("click", colorLink));
--- src/index.js
+++ src/index.js
@@ -1,121 +1,118 @@
-const { app, BrowserWindow, ipcMain, Menu, BrowserView } = require('electron');
-const url = require('url');
-const path = require('path');
+const { app, BrowserWindow, ipcMain, Menu, BrowserView } = require("electron");
+const url = require("url");
+const path = require("path");
 
-if(process.env.NODE_ENV !== 'production'){
-    require('electron-reload')(__dirname,{
-        electron: path.join(__dirname,'../node_modules','.bin','electron')
-    });
+if (process.env.NODE_ENV !== "production") {
+  require("electron-reload")(__dirname, {
+    electron: path.join(__dirname, "../node_modules", ".bin", "electron"),
+  });
 }
-
 
 let mainWindows;
 let newProductWindow;
 
-
-app.on('ready', () => {
-    mainWindows = new BrowserWindow({
-        webPreferences:{
-            nodeIntegration: true,
-            contextIsolation: false,
-            enableRemoteModule: true,
-        }
-    });
-    mainWindows.loadURL(url.format({
-        pathname: path.join(__dirname, 'views/index.htm'),
-        protocol: 'file',
-        slashes: true
-    }))
-    const mainMenu = Menu.buildFromTemplate(templateMenu);
-    Menu.setApplicationMenu(mainMenu);
-    mainWindows.maximize();
-    mainWindows.on('closed',()=>{
-        app.quit();
-    });
+app.on("ready", () => {
+  mainWindows = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+    },
+  });
+  mainWindows.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "views/index.htm"),
+      protocol: "file",
+      slashes: true,
+    })
+  );
+  const mainMenu = Menu.buildFromTemplate(templateMenu);
+  Menu.setApplicationMenu(mainMenu);
+  mainWindows.maximize();
+  mainWindows.on("closed", () => {
+    app.quit();
+  });
 });
 
 const templateMenu = [
-    {
-        label: 'File',
-        submenu: [
-            {
-                label: 'New Product',
-                accelerator: 'Ctrl-N',
-                click(){
-                    // createNewProduct();
-                 }
-            },
-            {
-                label: 'Remove All Products',
-        
-            },
-            {
-                label: 'Exit',
-                accelerator: process.platform == 'darwin' ? 'command+Q' : 'Ctrl+Q',
-                click(){
-                    app.quit();
-                }
-            }   
-        ]
-    }     
-]
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "New Product",
+        accelerator: "Ctrl-N",
+        click() {
+          // createNewProduct();
+        },
+      },
+      {
+        label: "Remove All Products",
+      },
+      {
+        label: "Exit",
+        accelerator: process.platform == "darwin" ? "command+Q" : "Ctrl+Q",
+        click() {
+          app.quit();
+        },
+      },
+    ],
+  },
+];
 
-if(process.platform !== 'darwin'){
-    templateMenu.unshift({
-        label: app.getName()
-    });
+if (process.platform !== "darwin") {
+  templateMenu.unshift({
+    label: app.getName(),
+  });
 }
 
-ipcMain.on('product:new', (e, newProduct) => {
-    mainWindows.webContents.send('product:new',newProduct);
-    newProductWindow.close();
+ipcMain.on("product:new", (e, newProduct) => {
+  mainWindows.webContents.send("product:new", newProduct);
+  newProductWindow.close();
 });
 
-function createNewProduct(){
+function createNewProduct() {
+  newProductWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    title: "Add a new product",
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+    },
+  });
+  //newProductWindow.setMenu(null);
+  newProductWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "views/persona.htm"),
+      protocol: "file",
+      slashes: true,
+    })
+  );
 
-
-    newProductWindow =  new BrowserWindow({
-        width:800,
-        height:600,
-        title: 'Add a new product',
-        webPreferences:{
-            nodeIntegration: true,
-            contextIsolation: false,
-            enableRemoteModule: true
-        }
-    });
-    //newProductWindow.setMenu(null);
-    newProductWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'views/persona.htm'),
-        protocol: 'file',
-        slashes: true
-    }))
-
-    newProductWindow.on('closed',()=>{
-        newProductWindow = null;
-    });
-
+  newProductWindow.on("closed", () => {
+    newProductWindow = null;
+  });
 }
 
-
-if(process.env.NODE_ENV !== 'production'){
-    templateMenu.push({
-        label: 'DevTools',
-        submenu: [
-            {
-                label: 'Show/Hide Dev Tools',
-                accelerator: 'Ctrl+D',
-                click(item, focusedWindow){
-                    focusedWindow.toggleDevTools();
-                }
-            },
-            {
-                role: 'reload'
-            }
-        ]
-    })
+if (process.env.NODE_ENV !== "production") {
+  templateMenu.push({
+    label: "DevTools",
+    submenu: [
+      {
+        label: "Show/Hide Dev Tools",
+        accelerator: "Ctrl+D",
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        },
+      },
+      {
+        role: "reload",
+      },
+    ],
+  });
 }
 
 module.exports = {
-    createNewProduct
-}
+  createNewProduct,
+};
