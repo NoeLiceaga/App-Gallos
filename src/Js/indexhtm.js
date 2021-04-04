8107b303ffb0bdc2587e674234affe0f5191d02c
NoeLiceaga <noe_tetraodron@outlook.com>
2021-04-03 19:16:03 -0600

    Actualizacion del Inicio

--- src/Js/indexhtm.js
+++ src/Js/indexhtm.js
@@ -7,38 +7,8 @@ const person = document.getElementById('person');
 person.addEventListener('click',addPerson)
 
 function addPerson(){
-    console.log('ji')
-    index.createNewProduct()
+    index.createNewProduct()    
 }
-
-// ipcRenderer.on('product:new',(e,newProduct) =>{
-//     const newProductTemplate = `
-//         <div class="col-xs-4 p-2">
-//             <div class="card text-center">
-//                 <div class="card-header">
-//                     <h5 class="card-title">${newProduct.name}</h5>
-//                 </div>
-//                 <div class="card-body">
-//                     ${newProduct.description}
-//                     <br>
-//                     ${newProduct.price} 
-//                 </div>
-//                 <div class="card-footer">
-//                     <button class="btn btn-danger btn-sm">
-//                         DELETE
-//                     </button>
-//                 </div>
-//             </div>
-//         </div>
-//     `;
-//     products.innerHTML += newProductTemplate;
-//     const btns = document.querySelectorAll('.btn.btn-danger');
-//     btns.forEach(btn => {
-//         btn.addEventListener('click',e =>{
-//             e.target.parentElement.parentElement.parentElement.remove();
-//         })
-//     });
-// });
 
 // ==== RESPONSIVE NAVBAR === //
  const showNavbar = (toggleId, navId, bodyId, headerId)=>{
